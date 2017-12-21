46e4a6a88ebca9c999d8f85f13837fd829bb6c4d
Paul Sczurek <pocketcalculatorshow@gmail.com>
2017-12-21 18:58:13 -0500

    updated to chain API communications together

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const ACCUWEATHER_CURRENT_CONDITIONS_URL = 'https://dataservice.accuweather.com/
 const MTB_PROJECT_API_KEY = '6523910-ecc597968f8123f52f6697bda4652415'
 const MTB_PROJECT_URL = 'https://www.mtbproject.com/data/get-trails'
 
-const lat = '36.0598781'
-const lon = '-84.3311592'
+const trails = []
 
-function getUserLocation(address,callback) {
+//const lat = '36.0598781'
+//const lon = '-84.3311592'
+
+function getUserLocation(address) {
   const settings = {
     url: GOOGLE_MAPS_GEOCODING_URL,
     data: {
@@ -18,13 +20,38 @@ function getUserLocation(address,callback) {
     },
     type: 'GET',
     dataType: 'JSON',
-    success: callback
+    success: function(data){
+      getMTBData(data.results[0].geometry.location)
+    }
   }
   $.ajax(settings)
 }
 
-function getAccuWeatherLocationKey(lat,lon,callback) {
-  const coordinates = lat + ',' + lon
+function getMTBData(location) {
+  const settings = {
+    url: MTB_PROJECT_URL,
+    data: {
+      key: MTB_PROJECT_API_KEY,
+      lat: location.lat,
+      lon: location.lng
+    },
+    type: 'GET',
+    dataType: 'JSON',
+    success: function(data) {
+      data.trails.forEach(function(trail, index, trails) {
+        const location = {
+          lat: trail.latitude,
+          lng: trail.longitude
+        }
+        getAccuWeatherLocationKey(location, trail, trails.length)
+      })
+    }
+  }
+  $.ajax(settings)
+}
+
+function getAccuWeatherLocationKey(location, trail, count) {
+  const coordinates = `${location.lat},${location.lng}`
   const settings = {
     url: ACCUWEATHER_GET_LOCATION_KEY_URL,
     data: {
@@ -33,46 +60,40 @@ function getAccuWeatherLocationKey(lat,lon,callback) {
     },
     type: 'GET',
     dataType: 'JSON',
-    success: callback
+    success: function(data) {
+      getRainData(data.Key, trail, count)
+    }
   }
   $.ajax(settings)
 }
 
-function getRainData(locationKey,callback) {
-  const ACCUWEATHER_RAIN_URL = ACCUWEATHER_CURRENT_CONDITIONS_URL + locationKey
+function getRainData(locationKey, trail, count) {
+  const accuweatherRainURL = `${ACCUWEATHER_CURRENT_CONDITIONS_URL}${locationKey}`
   const settings = {
-    url: ACCUWEATHER_RAIN_URL,
+    url: accuweatherRainURL,
     data: {
       apikey: ACCUWEATHER_API_KEY,
       details: true
     },
     type: 'GET',
     dataType: 'JSON',
-    success: callback
-  }
-  $.ajax(settings)
-}
-
-function getMTBData(lat,lon,callback) {
-  const settings = {
-    url: MTB_PROJECT_URL,
-    data: {
-      key: MTB_PROJECT_API_KEY,
-      lat,
-      lon
-    },
-    type: 'GET',
-    dataType: 'JSON',
-    success: callback
+    success: function(data) {
+      trail.precipitation = data[0].PrecipitationSummary.Past24Hours.Metric.Value
+      trails.push(trail)
+      if ( trails.length >= count ) {
+        console.log(trails)
+        console.log('Done!')
+      }
+    }
   }
   $.ajax(settings)
 }
 
 /* input address, retrieves lat & lon */
-getUserLocation(30328,console.log)
+getUserLocation(30328)
 /* input lat & lon, retrieves local bike trail list with lat & lon coordinates */
-getMTBData(lat,lon,console.log)
+// getMTBData(lat,lon,console.log)
 /* input lat & lon, retrieves Accuweather Location Key */
-getAccuWeatherLocationKey(lat,lon,console.log)
+// getAccuWeatherLocationKey({lat:lat,lng:lon})
 /* input Accuweather Location Key, retrieve weather data including precipitation stats for last 24 hours */
-getRainData(2121883,console.log)
+// getRainData(2121883,console.log)
