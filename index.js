27e87165eb3232e17722eade7ab4b48a36083d2f
Paul Sczurek <pocketcalculatorshow@gmail.com>
2017-12-27 12:03:32 -0500

    MVP that displays details of first trail in the trail array

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ const MTB_PROJECT_API_KEY = '6523910-ecc597968f8123f52f6697bda4652415'
 const MTB_PROJECT_URL = 'https://www.mtbproject.com/data/get-trails'
 
 const trails = []
+// name = name,
+// summary = summary,
+// img = imgSmall,
+// precipitation = precipitation
+
+const searchQuery = {
+  submittedAddress: [],
+  searchRadius: 10,
+  trailLength: 5
+}
 
 //const lat = '36.0598781'
 //const lon = '-84.3311592'
 
-function getUserLocation(address) {
+function getUserLocation(searchQuery) {
   const settings = {
     url: GOOGLE_MAPS_GEOCODING_URL,
     data: {
-      address,
+      address: searchQuery.submittedAddress,
       key:  GOOGLE_MAPS_GEOCODING_API_KEY
     },
     type: 'GET',
@@ -32,6 +42,8 @@ function getMTBData(location) {
     url: MTB_PROJECT_URL,
     data: {
       key: MTB_PROJECT_API_KEY,
+      maxDistance: searchQuery.searchRadius,
+      minLength: searchQuery.trailLength,
       lat: location.lat,
       lon: location.lng
     },
@@ -78,53 +90,45 @@ function getRainData(locationKey, trail, count) {
     type: 'GET',
     dataType: 'JSON',
     success: function(data) {
-      trail.precipitation = data[0].PrecipitationSummary.Past24Hours.Metric.Value
+      trail.precipitation = data[0].PrecipitationSummary.Past24Hours.Imperial.Value
       trails.push(trail)
       if ( trails.length >= count ) {
-        console.log(trails)
         console.log('Done!')
+        $( 'main' ).html(`<section id="map"></section>`)
+        initMap(trails)
       }
     }
   }
   $.ajax(settings)
 }
 
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+function initMap(trails) {
+  console.log(trails)
+  let trail1 = {lat: trails[0].latitude, lng: trails[0].longitude};
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: trail1
   });
 
-  var contentString = `<div id="content">
+  let trail1_contentString = `<div id="content">
       <div id="siteNotice">
       </div>
-      <h1 id="firstHeading" class="firstHeading">Uluru</h1>
+      <h1 id="firstHeading" class="firstHeading">${trails[0].name}</h1>
       <div id="bodyContent">
-      <p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large
-      sandstone rock formation in the southern part of the
-      Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi)
-      south west of the nearest large town, Alice Springs; 450&#160;km
-      (280&#160;mi) by road. Kata Tjuta and Uluru are the two major
-      features of the Uluru - Kata Tjuta National Park. Uluru is
-      sacred to the Pitjantjatjara and Yankunytjatjara, the
-      Aboriginal people of the area. It has many springs, waterholes,
-      rock caves and ancient paintings. Uluru is listed as a World
-      Heritage Site.</p>
-      <p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">
-      https://en.wikipedia.org/w/index.php?title=Uluru</a>
-      (last visited June 22, 2009).</p>
+      <p>${trails[0].summary}</p>
+      <img src="${trails[0].imgSmall}">
+      <p>${trails[0].precipitation} inches of precipitation in past 24 hours</p>
       </div>
       </div>`
 
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
+  let infowindow = new google.maps.InfoWindow({
+    content: trail1_contentString
   });
 
-  var marker = new google.maps.Marker({
-    position: uluru,
+  let marker = new google.maps.Marker({
+    position: trail1,
     map: map,
-    title: 'Uluru (Ayers Rock)'
+    title: `${trails[0].name}`
   });
   marker.addListener('click', function() {
     infowindow.open(map, marker);
@@ -135,12 +139,11 @@ function handleSearchButton() {
   console.log('Changing!')
   $( '#submit' ).click(function(){
     event.preventDefault()
-    let submittedAddress = $( '#address' ).val()
-    let searchRadius = $( '#searchRadius' ).val()
-    getUserLocation(submittedAddress)
+    searchQuery.submittedAddress = $( '#address' ).val()
+    searchQuery.searchRadius = $( '#searchRadius' ).val()
+    console.log(searchQuery)
+    getUserLocation(searchQuery)
   $( 'main' ).empty()
-  $( 'main' ).html(`<section id="map"></section>`)
-  initMap()
   })
 }
 
